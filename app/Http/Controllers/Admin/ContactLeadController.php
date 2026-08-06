ffa13d6d3b1ed1c080cdb95a11e0b72fbb1847d5
Ritikpatel963 <rp09444467@gmail.com>
2026-08-06 13:17:41 +0530

    Fix fatal error due to duplicate use statement in ContactLeadController

--- app/Http/Controllers/Admin/ContactLeadController.php
+++ app/Http/Controllers/Admin/ContactLeadController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\ContactLead;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
 class ContactLeadController extends Controller
 {
